803cee01a4928bff6b72bea51b2ff913cb4fe0a5
aptem9015 <79446061+aptem9015@users.noreply.github.com>
2022-11-20 22:00:30 +0200

    Update 5-Program-Methods-Using-Example.sql

--- 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
+++ 3-SoftwareConstruction/5-Program-Methods-Using-Example.sql
@@ -33,19 +33,19 @@ BEGIN
 END;
 /
 
-/* Приклад роботи з класами Architectural_project, Subscription, Article, User */
+/* Приклад роботи з класами Architectural_project, Subscription, Article, Users_ */
 DECLARE 
     ap1 Architectural_project;
     su1 Subscription;
     ar1 Article;
-    us1 User;
-    us2 User;
+    us1 Users_;
+    us2 Users_;
 BEGIN 
     ap1:= NEW Architectural_project(TO_PROJECT_NAME('Country villa'));
     su1:= Subscription(15, '13', 'Economical', '200', ap1);
     ar1 := Air_humidity(10, 'Business', su1);
-    us1 := User(35, 'Artem', 'Babich', 'artem.babich@gmail.com', su1, ar1);
-    us2 := User(46, 'Nikita', 'Frolov', 'nikita.frolov@gmail.com', su2, ar2);
+    us1 := Users_(35, 'Artem', 'Babich', 'artem.babich@gmail.com', su1, ar1);
+    us2 := Users_(46, 'Nikita', 'Frolov', 'nikita.frolov@gmail.com', su2, ar2);
     us1.display(); 
     us2.display(); 
 END;
